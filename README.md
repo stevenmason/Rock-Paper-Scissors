5e68c6836294f3108422fe377fb3e102e76bac23
James Wimmers <jimmyskydive@gmail.com>
2023-06-08 19:56:02 +1200

    changed script src to app.js and created rock, paper, scissors choice function

---
+++ README.md
@@ -0,0 +1,3 @@
+This application is a game of rock paper scissors.
+
+A classic game for everyone.
---
+++ app.js
@@ -0,0 +1,22 @@
+function getComputerChoice(computerNumber) {
+if (computerNumber === 1) {
+    return "Rock"
+}
+else if (computerNumber === 2) {
+    return "Paper"
+}
+else return "Scissors"
+}
+
+
+
+
+/*function playRound(playerSelection, computerSelection) {
+
+  }
+   
+  
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  */
