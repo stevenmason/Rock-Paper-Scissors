e6fde11fe66a4ac79d55f24449f2387ace974509
James Wimmers <jimmyskydive@gmail.com>
2023-06-08 20:39:29 +1200

    made random selection work for the cmputer choice

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-function getComputerChoice(computerNumber) {
+const getComputerChoice = function (computerNumber) { // once random number is selected a hand is chosen
 if (computerNumber === 1) {
     return "Rock"
 }
 else if (computerNumber === 2) {
     return "Paper"
 }
-else return "Scissors"
+else {
+    return "Scissors"
 }
+}
+const computerSelection = getComputerChoice(Math.floor((Math.random()*3)+1)); // chooses a random number between 1-3
 
-
+console.log(computerSelection)
 
 
 /*function playRound(playerSelection, computerSelection) {
@@ -17,6 +20,6 @@ else return "Scissors"
    
   
   const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
+
   console.log(playRound(playerSelection, computerSelection));
   */
